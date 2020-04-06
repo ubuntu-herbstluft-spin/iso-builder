d4f7aa92e0b61903a575b0c938e78f7b55ea0099
Kai Lyons <62919224+KaiLikesLinux@users.noreply.github.com>
2020-04-05 20:15:25 -0600

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -89,7 +89,7 @@ build () {
     YYYYMMDD="$(date +%Y%m%d)"
     OUTPUT_DIR="$BASE_DIR/builds/$BUILD_ARCH"
     mkdir -p "$OUTPUT_DIR"
-    FNAME="cinnamonremix-$VERSION-$CHANNEL.$YYYYMMDD$OUTPUT_SUFFIX"
+    FNAME="ubuntulumina-$VERSION-$CHANNEL.$YYYYMMDD$OUTPUT_SUFFIX"
     mv "$BASE_DIR/tmp/$BUILD_ARCH/live-image-$BUILD_ARCH.hybrid.iso" "$OUTPUT_DIR/${FNAME}.iso"
 
     md5sum "$OUTPUT_DIR/${FNAME}.iso" > "$OUTPUT_DIR/${FNAME}.md5.txt"
